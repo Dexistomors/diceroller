2a9e8602c8648ae4a2a484b233ffe38a0026afeb
Mark Johnson <dexistomors@yahoo.com>
2024-05-14 13:50:15 -0400

    added to die and test_die

--- tests/test_die.py
+++ tests/test_die.py
@@ -1,5 +1,6 @@
 import unittest
 from context import diceroller
+from unittest.mock import patch
 
 class TestDie(unittest.TestCase):
     def test_init_die(self):
@@ -30,3 +31,30 @@ class TestDie(unittest.TestCase):
             advantage = 0
             reroll_rules = {}
             self.assertRaisesRegex(diceroller.DieException, "reroll_rules has incorrect data format")
+        
+        def randint_min(min, max):
+            return min
+        def randint_max(min, max):
+            return max
+        
+        with patch('random.randint', randint_min), \
+            self.subTest("correct minimum roll and reset"):
+            faces = 20
+            test_die = diceroller.Die(faces)
+            test_die.roll()
+            self.assertEqual([1], test_die.get_die_result())
+            test_die.roll()
+            self.assertEqual([1, 1], test_die.get_die_result())
+            test_die.reset()
+            self.assertEqual(None, test_die.get_die_result())
+            
+        with patch('random.randint', randint_max), \
+            self.subTest("correct maximum roll and reset"):
+            faces = 20
+            test_die = diceroller.Die(faces)
+            test_die.roll()
+            self.assertEqual([20], test_die.get_die_result())
+            test_die.roll()
+            self.assertEqual([20, 20], test_die.get_die_result())
+            test_die.reset()
+            self.assertEqual(None, test_die.get_die_result())
