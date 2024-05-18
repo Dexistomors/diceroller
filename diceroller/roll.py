f0d16d808f81e7da2524289fd2fe4106c202eda4
Mark Johnson <dexistomors@yahoo.com>
2024-05-18 13:56:33 -0400

    adding into roll.py and creating tests for roll.py

--- diceroller/roll.py
+++ diceroller/roll.py
@@ -1,19 +1,47 @@
 import uuid
+from diceroller.die import Die
+from diceroller.rollresult import RollResult
 
 class Roll:
 
     def __init__(self, dice, modifiers=[]):
         self.id = uuid.uuid4()
-        self.dice = dice
-        self.modifiers = modifiers
+        self.dice = Roll._validate_dice(dice)
+        self.modifiers = Roll._validate_modifiers(modifiers)
         self.roll_result = None
         self.final_value = None
+
+    def _validate_dice(dice):
+        if type(dice) != list:
+            raise RollException("dice has incorrect data format")
+        for die in dice:
+            Roll._validate_die(die)
+        return dice
+
+    def _validate_die(die):
+        if type(die) == Die:
+            return die
+        else:
+            raise RollException("die not Die")
+        
+    def _validate_modifiers(modifiers):
+        if type(modifiers) != list:
+            raise RollException("modifier has incorrect data")
+        for modifier in modifiers:
+            Roll._validate_modifier(modifier)
+        return modifiers
+
+    def _validate_modifier(modifier):
+        if type(modifier) == int:
+            return modifier
+        else:
+            raise RollException("modifier not int")
 
     def create_dice_from_dice_configs(self, dice_configs):
         pass
 
     def add_die(self, die):
-        pass
+        self.dice.append(Roll._validate_die(die))
 
     def remove_die(self, die):
         pass
@@ -22,7 +50,7 @@ class Roll:
         pass
 
     def add_modifier(self, modifier):
-        pass
+        self.modifiers.append(Roll._validate_modifier)
 
     def remove_modifier(self, modifier):
         pass
@@ -31,7 +59,14 @@ class Roll:
         pass
 
     def roll(self):
-        pass
+        dice_final_results = []
+        dice_final_value = sum(self.modifiers)
+        for die in self.dice:
+            diceresult = die.roll()
+            dice_final_value = dice_final_value + diceresult.get_final_value()
+            dice_final_results.append(diceresult)
+        self.roll_result = RollResult(dice_final_results, self.modifiers, dice_final_value)
+        self.final_value = dice_final_value
 
     def reset(self):
         pass
@@ -41,3 +76,6 @@ class Roll:
 
     def get_final_result(self):
         pass
+
+class RollException(Exception):
+    pass
--- diceroller/rollresult.py
+++ diceroller/rollresult.py
@@ -2,11 +2,11 @@ import uuid
 
 class RollResult:
 
-    def __init__(self, dice_results, modifiers = []):
+    def __init__(self, dice_results, modifiers, total):
         self.id = uuid.uuid4()
         self.dice_results = dice_results
         self.modifiers = modifiers
-        self.total = None
+        self.total = total
 
     def serialize(self):
         pass
