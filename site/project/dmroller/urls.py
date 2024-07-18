d6ca2d1887bb23013d82904ca47318eff9eb0bde
Mark <dexistomors@yahoo.com>
2024-07-18 18:11:34 +0000

    continuing css styling, added room tile and dove tile at bottom

---
+++ site/project/dmroller/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name = "index"),
+    path("api/roll/", views.api_roll, name = "api_roll"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("roller/", views.roller, name = "roller"),
+]
---
+++ site/project/dmroller/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.template import loader
+import diceroller
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(None, request))
+
+def login(request):
+    template = loader.get_template('registration/login.html')
+    return HttpResponse(template.render(None, request))
+
+@login_required
+def api_roll(request):
+    data = request.POST.get("roll_config")
+    try:
+        roll = diceroller.Roll.deserialize(data)
+        result = 'Dice result: {}'.format(roll.roll().serialize())
+    except Exception as error:
+        result = 'Encountered an error {}'.format(error)
+        
+    return HttpResponse("Result: {}".format(result))
+
+@login_required
+def roller(request):
+    template = loader.get_template('roller.html')
+    return HttpResponse(template.render(None, request))
