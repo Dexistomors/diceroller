d82402cc159e2b50be64be1e208bc9e1b7020c99
Mark <dexistomors@yahoo.com>
2024-07-20 16:17:10 +0000

    Added in base for database handling and class creation

--- site/project/dmroller/models.py
+++ site/project/dmroller/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.auth import get_user_model
+import uuid
 # Create your models here.
+
+class Room(models.Model):
+    room_code = models.CharField(max_length=8,blank=True,null=True,unique=True)
+    def save(self, *args, **kwargs):
+        while not self.room_code:
+            temp = uuid.uuid4().hex[:8].upper()
+            if not Room.objects.filter(room_code=temp).exclude(pk=self.pk).exists():
+                self.room_code = temp
+                break
+        super(Room, self).save(*args, **kwargs)
+
+class RoomUser(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+class RollConfig(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    roll_config = models.TextField()
+
+class RollResult(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    roll_config = models.ForeignKey(RollConfig, on_delete=models.CASCADE)
+    roll_result = models.TextField()
